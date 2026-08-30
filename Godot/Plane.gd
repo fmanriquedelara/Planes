d9415356ac69dcb119fb20f57b4137c83d29f6b8
fmanriquedelara <f.manriquedelara@hotmail.com>
2026-08-30 18:04:29 +0100

    New project

---
+++ Godot/Plane.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5a561074598d2146f68598f59c38219be53a171e5a03f1005172494af809701b
+size 1484
---
+++ Godot/shader.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5abd9193696bff9553d304021ccebf725e17443b3267d6c62661faf4e4129585
+size 1615
