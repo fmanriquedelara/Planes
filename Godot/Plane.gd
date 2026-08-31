2f6f2f94ce589b39f113cac0a735bc44329210b3
fmanriquedelara <f.manriquedelara@hotmail.com>
2026-08-31 17:30:47 +0100

    Weight + yaw + Engine addition

--- Godot/Plane.gd
+++ Godot/Plane.gd
@@ -1,20 +1,29 @@
 extends Node3D
 
 var time := 0.0
-var V := 150.0
+var V := 100.0
 
 var attitude := Quaternion.IDENTITY
 var omega := Vector3.ZERO
 
+var Cx = 0.8;
+var FEngineMax = 10000;
+var FEngineMean = 5000;
+
+var m = 100;
+
+var g = 9.81;
+
 # Time constants [seconds]
 var tau_roll := 0.1
 var tau_pitch := 0.2
+var tau_yaw := 0.2
 @onready var engine_sound: AudioStreamPlayer3D = $EngineSound
 
 func _ready():
 	position = Vector3(0, 10, 0)
 	engine_sound.play()
-	engine_sound.volume_db = -10.0
+	engine_sound.volume_db = -4.0
 
 
 func _process(delta):
@@ -31,18 +40,40 @@ func _process(delta):
 	
 	var throttle = Input.get_axis("rBrake", "rEngine")
 
-	engine_sound.volume_db = lerp(-20.0, 0.0, 1)
+	engine_sound.volume_db = lerp(-8.0, 0.0, throttle)
 
 
 func _update_dynamics(delta):
 	var pitch = Input.get_axis("aPitchIncrease", "aPitchDecrease")
 	var roll = Input.get_axis("aRolLeft", "aRollRight")
-
+	var left = Input.get_action_strength("aRolLeft")
+	var right = Input.get_action_strength("aRollRight")
+	var rEngine = Input.get_axis("rBrake", "rEngine")
+	var yaw   = Input.get_axis("YawLeft", "YawRight")
+	
+	var Flift = 0.5 * 1.25 * (V*V) * 0.000167 * pitch;
+	var FLat = 0;
+	var MRoll = 0.5 * 1.25 * (V*V) * 0.000167 * roll;
+	
+	var weight = m * g;
+	
+	var Fweight_x = - weight * basis.x.y;
+	var Fweight_y = 0;
+	var Fweight_z = weight * basis.z.y;
+	
+	
 	var safe_V = max(V, 0.01)
+	
+	var F_drag = 0.5*1.25 *Cx*V*V;
+	var F_engine = FEngineMean + (FEngineMax - FEngineMean) * rEngine;	
+	var FYaw = 30 * yaw + 0.005*Fweight_z;
 
 	# Desired angular rates
-	var omega_x_cmd = -6.0 * roll
-	var omega_z_cmd = 200.0 * pitch / safe_V
+	var omega_x_cmd = -6.0 * MRoll
+	var omega_z_cmd = 200.0 * Flift / safe_V
+	var omega_y_cmd = - FYaw / safe_V;
+	
+	print(basis.z.y)
 
 	# First-order roll dynamics
 	var roll_alpha = 1.0 - exp(-delta / tau_roll)
@@ -53,7 +84,12 @@ func _update_dynamics(delta):
 	omega.z += (omega_z_cmd - omega.z) * pitch_alpha
 
 	# No yaw for now
-	omega.y = 0.0
+	var yaw_alpha = 1.0 - exp(-delta / tau_yaw)
+	omega.y += (omega_y_cmd - omega.y) * yaw_alpha
+	
+	V += delta * (F_engine-F_drag-Fweight_x) / m;
+	
+	print(V)
 
 
 func _integrate_attitude(delta):
