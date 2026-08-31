47df331b4e03c3988f85389e51b108e6dc12de2d
fmanriquedelara <f.manriquedelara@hotmail.com>
2026-08-31 18:36:29 +0100

    Fix textures

--- Godot/Plane.gd
+++ Godot/Plane.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca3910419e9dce7e50fedbcba43e179949d4a9499993b9a5c0e0a67134f887ed
-size 4920
+oid sha256:2145d13c8f6490117fe82c4ebb6b8a58b04e313513b4914d81028ded91552835
+size 5446
