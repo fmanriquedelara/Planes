993ce0e2daac991afb1cae6960507435a61c9549
fmanriquedelara <f.manriquedelara@hotmail.com>
2026-08-31 18:29:30 +0100

    Texture back on

--- Godot/Plane.gd
+++ Godot/Plane.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34cd91ce51312144eadb925b3c5522e73de3acf5abb47324d7bf07b956498a44
-size 4058
+oid sha256:ca3910419e9dce7e50fedbcba43e179949d4a9499993b9a5c0e0a67134f887ed
+size 4920
