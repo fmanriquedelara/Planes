57acadef463b029c4a8a790d2d24e2899544d0dd
fmanriquedelara <f.manriquedelara@hotmail.com>
2026-09-19 13:37:33 +0100

    Ignore blender files + camera logic

--- Godot/Plane.gd
+++ Godot/Plane.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f0f35fa0fd43fd3922f72abd9406a385fd56ce16e98d98d149bb6fb71507e74
-size 5979
+oid sha256:5f4476276ba3de95c61ae323e6d30166ad7d278f54173800d2b9c59894e30698
+size 6380
